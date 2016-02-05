3553f87bf4b661715fd21819c69046cd056e2a71
Kiana Tennyson <ktenny@thoughtworks.com>
2016-02-05 15:52:41 -0500

    CORS for all

--- mongoserver.js
+++ mongoserver.js
@@ -27,7 +27,7 @@ mongoClient.connect('mongodb://localhost:27017/motionsensor', function(err, db) 
 });
 
 router.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://atliot.com:4666");
+  res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
